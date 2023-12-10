f4eb199cce6350c594a3090afb6fb333dd146da0
JHugoSilva <hugosilvahs31@gmail.com>
2023-12-10 00:14:47 -0300

    Start API

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function register(Request $request) {
+        
+    }
+
+    public function login(Request $request) {
+        
+    }
+
+    public function profile(Request $request) {
+        
+    }
+
+    public function logout(Request $request) {
+        
+    }
+}
